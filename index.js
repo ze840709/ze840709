755ba4fd5ab4ab55ae1349c285af0c4b9a2369e3
ze840709 <93631416+ze840709@users.noreply.github.com>
2021-11-12 14:21:53 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home(){
+	return(
+		<div>
+			<button className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">connect to MetaMask</button>
+			<span>Not connected</span>
+		</div>
+	)
+}
